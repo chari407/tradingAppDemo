4c0a83fa16c42742af313625211f328b2c38ff6a
chari407 <amareshiragavarapu@gmail.com>
2022-10-11 12:10:44 +0200

    INitial Project Setup added

---
+++ src/main/java/com/db/tradingAppDemo/TradingAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.db.tradingAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/tradingAppDemo/TradingAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.tradingAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
